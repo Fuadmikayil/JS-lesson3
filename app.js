b53cd634df2242580325e076c38c8d3b38198d2d
Fuad <mikayilov2006@gmail.com>
2024-07-07 10:36:48 +0400

    if+prompt

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = prompt("Enter number a...")
+let b = prompt("Enter number b...")
+if(a>b){
+    alert(`${a} > ${b}`)
+}
+if(a<b){
+    alert(`${a} < ${b}`)
+}
+if(a==b){
+    alert(`${a} == ${b}`)
+}
