da94d5435df25f2a6a8c9d88db0ac57bd039c48e
Fuad <mikayilov2006@gmail.com>
2024-07-07 10:46:53 +0400

    else +info

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ else if(a<b){
 else if(a==b){
     alert(`${a} == ${b}`)
 }
+else{
+    alert("Wrong Number")
+}
 ////////if bloku her zaman oxunur 
 //////// else if,else bloku  if blokuna qeder butun bloklar  false olanda oxunur
 //if, else if de şert yazılır lakin else-de şert olmur
