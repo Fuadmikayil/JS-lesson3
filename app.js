82d377f5ad2a4e3b53bde2bd01feb159f44a1467
Fuad <mikayilov2006@gmail.com>
2024-07-07 10:41:16 +0400

    else if +info

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ let b = prompt("Enter number b...")
 if(a>b){
     alert(`${a} > ${b}`)
 }
-if(a<b){
+else if(a<b){
     alert(`${a} < ${b}`)
 }
-if(a==b){
+else if(a==b){
     alert(`${a} == ${b}`)
 }
-////////if bloku her zaman oxunur ve şert  yoxlanılır
+////////if bloku her zaman oxunur 
+//////// else if bloku  if blokuna qeder butun bloklar  false olanda oxunur
