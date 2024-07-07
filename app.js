621f9532e8f7ffb0e4191e796ab208e9e6369bd5
Fuad <mikayilov2006@gmail.com>
2024-07-07 12:35:27 +0400

    Redbull +Cola bazarstore

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ else {
 //if, else if de şert yazılır lakin else-de şert olmur
 
 //Level 1 -mini calculuyator
-
+/*
 let sec = prompt("Zehmet olmasa seçim edin + - * / ");
 
 let num1 = +prompt("1ci reqemi daxil edin");
@@ -36,4 +36,20 @@ if (sec === '+') {
 
 } else {
     alert("Yanlış seçim daxil etdiniz!!!!!!!!!!");
+}*/
+
+
+alert("Redbull - 1.20azn, Cola 2.30, Sprite 4.10 ")
+let sec = prompt("Redbulldan neçe dene olsun?");
+let sec2 = prompt("Cola neçe dene olsun?");
+let sec3 = prompt("Sprite neçe dene olsun?");
+
+let result =1.20*sec+2.30*sec2+4.10*sec3 
+alert(result.toFixed(2)+"azn");
+let money = prompt("Ödenişi edin !!!!!!")
+if(money>result.toFixed(2)){
+    alert(money-result.toFixed(2) +"azn")
+}
+else{
+    alert("Kasıb!!!")
 }
