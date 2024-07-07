52f6b076e9a2eb67a93e6941875561285fe9e2c9
Fuad <mikayilov2006@gmail.com>
2024-07-07 11:50:24 +0400

    task 1 mini calc

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-let a = prompt("Enter number a...")
+/*let a = prompt("Enter number a...")
 let b = prompt("Enter number b...")
-if(a>b){
+if (a > b) {
     alert(`${a} > ${b}`)
 }
-else if(a<b){
+else if (a < b) {
     alert(`${a} < ${b}`)
 }
-else if(a==b){
+else if (a == b) {
     alert(`${a} == ${b}`)
 }
-else{
+else {
     alert("Wrong Number")
-}
+}*/
 ////////if bloku her zaman oxunur 
 //////// else if,else bloku  if blokuna qeder butun bloklar  false olanda oxunur
 //if, else if de şert yazılır lakin else-de şert olmur
+
+//Level 1 -mini calculuyator
+
+let sec = prompt("Zehmet olmasa seçim edin + - * / ");
+
+let num1 = +prompt("1ci reqemi daxil edin");
+let num2 = +prompt("2ci reqemi daxil edin");
+if (sec === '+') {
+    alert(num1 + num2);
+
+} else if (sec === '-') {
+    alert(num1 - num2);
+
+} else if (sec === '*') {
+    alert(num1 * num2);
+} else if (sec === '/') {
+    alert(num1 / num2);
+
+
+} else {
+    alert("Yanlış seçim daxil etdiniz!!!!!!!!!!");
+}
